6fb0dac883c8e65aff81e17899889adda8cd8f83
kieranfitz <pte_fitz@hotmail.com>
2018-03-18 23:37:13 +0000

    added routing for add,update,delete contact

--- part2/index.js
+++ part2/index.js
@@ -1,12 +1,17 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import gamesRouter from './api/games';
+import bodyParser from 'body-parser';
 
 dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT;
+
+//configure body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 app.use(express.static('public'));
 
